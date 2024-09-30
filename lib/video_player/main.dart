b231a8ad28359f062f46180f0251d6ae6afbf08a
Jboy422 <gitonly543@gmail.com>
2024-10-01 07:40:30 +0900

    added: video

--- lib/video_player/main.dart
+++ lib/video_player/main.dart
@@ -3,55 +3,77 @@ import 'package:video_player/video_player.dart';
 
 void main() => runApp(const VideoApp());
 
-/// Stateful widget to fetch and then display video content.
-class VideoApp extends StatefulWidget {
+class VideoApp extends StatelessWidget {
   const VideoApp({super.key});
 
   @override
-  _VideoAppState createState() => _VideoAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Video Demo',
+      home: VideoPlayerScreen(),
+    );
+  }
 }
 
-class _VideoAppState extends State<VideoApp> {
+class VideoPlayerScreen extends StatefulWidget {
+  const VideoPlayerScreen({super.key});
+
+  @override
+  VideoPlayerScreenState createState() => VideoPlayerScreenState();
+}
+
+class VideoPlayerScreenState extends State<VideoPlayerScreen> {
   late VideoPlayerController _controller;
+  bool _isInitialized = false;
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.networkUrl(Uri.parse(
-        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'))
-      ..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-        setState(() {});
+    _initializeVideoPlayer();
+  }
+
+  Future<void> _initializeVideoPlayer() async {
+    _controller = VideoPlayerController.networkUrl(
+      Uri.parse('https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'),
+    );
+
+    try {
+      await _controller.initialize();
+      setState(() {
+        _isInitialized = true;
       });
+    } catch (error) {
+      print('Error initializing video player: $error');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Video Demo',
-      home: Scaffold(
-        body: Center(
-          child: _controller.value.isInitialized
-              ? AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: VideoPlayer(_controller),
-                )
-              : Container(),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _controller.value.isPlaying
-                  ? _controller.pause()
-                  : _controller.play();
-            });
-          },
-          child: Icon(
-            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('async/await'),
+      ),
+      body: Center(
+        child: _isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : const CircularProgressIndicator(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _togglePlayPause,
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
         ),
       ),
     );
+  }
+
+  void _togglePlayPause() {
+    setState(() {
+      _controller.value.isPlaying ? _controller.pause() : _controller.play();
+    });
   }
 
   @override
